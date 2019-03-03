0f4fcc071657ec8104493fb50d26a487796eccd4
ajcper <james@perlego.com>
2019-03-03 16:59:47 +0000

    build init

---
+++ precache-manifest.6c03ca7453d23e4b2c8f4664b9b053ba.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3be96cbbfd0ff1f3ba3c7c2f58c1441",
+    "url": "/static/media/Roboto-Regular-webfont.b3be96cb.woff"
+  },
+  {
+    "revision": "006de26aa4cfbeaf5c91",
+    "url": "/static/css/main.fde5904f.chunk.css"
+  },
+  {
+    "revision": "4972e0391bcf2bb2e641",
+    "url": "/static/js/1.4972e039.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa805d0546640e574448106e9d61e8ff",
+    "url": "/static/media/Canela-Bold-Web.aa805d05.eot"
+  },
+  {
+    "revision": "b7c5fa0f65a480fbe2831c3c6044581b",
+    "url": "/static/media/Canela-Bold-Web.b7c5fa0f.woff2"
+  },
+  {
+    "revision": "afa606b71de673f7701dd7b3bf359968",
+    "url": "/static/media/Canela-Bold-Web.afa606b7.woff"
+  },
+  {
+    "revision": "6de9e7e891343e0237c9b4a2c6eaab4d",
+    "url": "/static/media/Roboto-Regular-webfont.6de9e7e8.eot"
+  },
+  {
+    "revision": "006de26aa4cfbeaf5c91",
+    "url": "/static/js/main.006de26a.chunk.js"
+  },
+  {
+    "revision": "f4defaab99fc32df94696ca1def0c849",
+    "url": "/static/media/Roboto-Medium-webfont.f4defaab.eot"
+  },
+  {
+    "revision": "d3e528d9ca58d216b29710f06b339f9d",
+    "url": "/static/media/Roboto-Medium-webfont.d3e528d9.woff"
+  },
+  {
+    "revision": "b67b57ee8df108bdf0af63b1feb48e1e",
+    "url": "/static/media/Roboto-Bold-webfont.b67b57ee.eot"
+  },
+  {
+    "revision": "388938f6332e5e07466341f141effc6c",
+    "url": "/static/media/Roboto-Bold-webfont.388938f6.woff"
+  },
+  {
+    "revision": "474699b1eef5a6fd0d7f34a4edf09f5d",
+    "url": "/static/media/Roboto-Light-webfont.474699b1.eot"
+  },
+  {
+    "revision": "f0fc91f2311b5251806eefac5bd03fa1",
+    "url": "/static/media/Roboto-Light-webfont.f0fc91f2.woff"
+  },
+  {
+    "revision": "26b44abdf5c77dfd88e8cc8a58efc813",
+    "url": "/index.html"
+  }
+];
